06835c0f6940b283ef10f834d686a3535a0b9f98
John Desautels <cluesnomads@gmail.com>
2025-11-21 08:37:11 +0100

    Move Search Neighborhood closer to Time Travel slider (85px from bottom)

--- src/lib/property-search-widget.js
+++ src/lib/property-search-widget.js
@@ -72,7 +72,7 @@ export class PropertySearchWidget {
         <style>
             .search-widget {
                 position: fixed;
-                bottom: 180px;
+                bottom: 85px;
                 left: 50%;
                 transform: translateX(-50%);
                 z-index: 1000;
